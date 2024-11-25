fcdbff9e7180b2481ac7703d37242c5b2c32beef
javierR26 <javiecardenas128@gmail.com>
2024-11-24 19:57:21 -0500

    Cuarto commit cambie algunas cosa como css y js y algunas funcionalidades y agrege mas imagenes

---
+++ js/botonemym.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('.masymenos').on('click', function () {
+        let $input = $('.masymenos-input');
+        $input.attr('type', 'number');
+        let currentValue = parseInt($input.val() || $input.attr('placeholder')) || 0;
+
+        if (currentValue > 0) {
+            $input.val(currentValue - 1);
+        }
+
+        $input.attr('type', 'text'); 
+    });
+
+    $('.masymenos1').on('click', function () {
+        let $input = $('.masymenos-input');
+        $input.attr('type', 'number');
+        let currentValue = parseInt($input.val() || $input.attr('placeholder')) || 0;
+
+        let maxValue = parseInt($('#Disponibles').text()) || 0;
+
+        if (currentValue < maxValue) {
+            $input.val(currentValue + 1);
+        }
+
+        $input.attr('type', 'text'); 
+    });
+});
--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-fetch("http://localhost:8080/getProductoById/5", {
+
+
+fetch("http://localhost:8080/getProductoById/10", {
   method: 'GET'
 })
   .then(response => {
@@ -8,12 +10,13 @@ fetch("http://localhost:8080/getProductoById/5", {
     return response.json(); // Asume que el servidor devuelve JSON
   })
   .then(data => {
-    console.log(data); // Muestra la respuesta en la consola stockProducto
+    console.log(data); // Muestra la respuesta en la consola stockProducto  nombre_Probucto
+    document.getElementById("nombre_Probucto").textContent = data.nombreProducto;
     document.getElementById("nombreProbucto").textContent = data.nombreProducto;
     document.getElementById("Precio").textContent = data.precioProducto;
     document.getElementById("Descripcion").textContent = data.descripcionProducto;
     document.getElementById("Disponibles").textContent = data.stockProducto;
- 
+    document.getElementById("imagen").src = data.imagenProducto;
   })
   .catch(error => {
     console.error('Error al obtener el producto:', error);
